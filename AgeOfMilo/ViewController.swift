59b5600721a44ca0545a6d2ab02ac84b7c4c64ad
Will Lam <will@willlam.net>
2015-02-02 16:33:09 -0500

    Initial Commit

---
+++ AgeOfMilo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgeOfMilo
+//
+//  Created by William Lam on 2015-02-02.
+//  Copyright (c) 2015 William Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
